30d6aacb2f066945ce94d52ceb179721f77fb922
Benjamin Mayr <b.mayr@cognigy.com>
2020-02-25 11:31:45 +0100

    Add '--version' argument to initialization tool. Execute actual functionality only if '--generate' argument is used - render '--help' output by default.

--- initialization/src/entrypoints/help.ts
+++ initialization/src/entrypoints/help.ts
@@ -4,15 +4,28 @@ import * as commandLineUsage from "command-line-usage";
 /** Define usage information */
 const sections: commandLineUsage.Section[] = [
 	{
-		header: 'Initialization script for Cognigy.AI and kubernetes (k8s)',
-		content: 'Helps with the initial installation process of a Cognigy.AI installation on top of Kubernetes.'
+		header: "Initialization script for Cognigy.AI and kubernetes (k8s)",
+		content: "Helps with the initial installation process of a Cognigy.AI installation on top of Kubernetes."
 	},
 	{
-		header: 'Options',
+		header: "Options",
 		optionList: [
 			{
-				name: 'help',
-				description: 'Prints this usage guide.'
+				name: "help",
+				alias: "h",
+				description: "Prints this usage guide."
+			},
+			{
+				name: "version",
+				description: "Shows the version of the initialization tool you are currently using."
+			},
+			{
+				name: "generate",
+				alias: "g",
+				description: `Generates a copy of the 'secrets.dist' folder in
+				the same working directory - named 'secrets'. Fills all secrets
+				in this new folder with secure random-data. You might want to adjust
+				certain secrets before you apply them to your cluster.`
 			}
 		]
 	}
@@ -21,5 +34,6 @@ const sections: commandLineUsage.Section[] = [
 export function renderHelp() {
 	const usage = commandLineUsage(sections);
 	console.log(usage);
+
 	process.exit(0);
 }
